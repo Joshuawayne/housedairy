44ddb24df0eae9e5d55a1ccfc14c466947accf3d
joshua mercy <47858381+Joshuawayne@users.noreply.github.com>
2025-03-04 12:14:20 +0300

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    supabaseUrl: 'https://qittolrihkbgbdaxtttv.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpdHRvbHJpaGtiZ2JkYXh0dHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MTE3OTEsImV4cCI6MjA1NjM4Nzc5MX0.tN0KMUB4UCwflH3CKNS7VAH5cp-QzxZAjCnJpdub8jA'   // Replace with your Supabase Anon Key
+    supabaseUrl: 'https://qi.supabase.co',// Replace with your Supabase url
+    supabaseKey: 'JkYXh0dQzxZAjCnJpdub8jA'   // Replace with your Supabase Anon Key
 };
